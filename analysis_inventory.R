05c5b9c83547f99b6938314bdf9188fabd03faef
Burak Karakan <burak.karakan@boosmart.com>
2017-05-20 13:23:19 +0300

    Cleaned up the repo.

--- analysis_inventory.R
+++ analysis_inventory.R
@@ -1,30 +1,18 @@
 rm(list = ls())
-library(smooth)
-library(ggplot2)
-library(gtools)
-library(dplyr)
-library(data.table)
 library(zoo)
+library(data.table)
 
-Mode <- function(x) {
-  ux <- unique(x)
-  ux[which.max(tabulate(match(x, ux)))]
-}
-
+# Define the IO paths.
 inputFilePath <- 'rdata/InventoryPosition.csv'
-outputFİlePath <- 'rdata/ForecastResults.csv'
+outputFİlePath <- 'rdata/ForecastResults_FoolsOnParade.csv'
 
 # Read the data from csv.
 inventoryData <- fread(inputFilePath, header = TRUE, dec = ".", sep = '|')
-inventoryData$Date <- as.Date(inventoryData$Date)
+inventoryData$Date <- as.Date(inventoryData$Date, format="%d.%m.%Y")
 
-#inventoryData <- inventoryData[StoreStock != 0 | SalesQuantity != 0]
-#
-inventoryData[, c("StoreStock","IncomingStock"):=NULL]
+# Drop the unused rows.
+inventoryData[, c("SalesRevenue","IncomingStock"):=NULL]
 row.names(inventoryData) <- 1:nrow(inventoryData)
-
-# Filter the data to a smaller subset in order to test the results.
-inventoryData <- inventoryData[StoreCode == 'Store1']
 
 # Construct a date vector.
 startDate <- as.Date("2015-01-1")
@@ -44,7 +32,10 @@ inventoryData <- merge(inventoryData, merged,
                        all=T)
 
 # Sort the data.
-inventoryData <- inventoryData[order(inventoryData$StoreCode, inventoryData$ProductCode, inventoryData$Date),]
+inventoryData <- inventoryData[
+	order(inventoryData$StoreCode, inventoryData$ProductCode, inventoryData$Date),]
+
+# Expand the dataset with the last observed carried forward method.
 inventoryData$StoreStock <- na.locf(inventoryData$StoreStock)
 
 # Convert NAs to zero.
@@ -52,20 +43,26 @@ inventoryData[is.na(inventoryData)] <- 0
 inventoryData <- data.table(inventoryData)
 inventoryData <- inventoryData[order(inventoryData$StoreCode, inventoryData$ProductCode, inventoryData$Date),]
 
+# Filter the dataset in order to eliminate zero sales and zero stock days.
+inventoryData <- inventoryData[SalesQuantity >= 0 & StoreStock >= 0]
+inventoryData <- inventoryData[!(SalesQuantity == 0 & StoreStock == 0)]
+inventoryData <- data.table(inventoryData)
+
 # Get the predictions for the next 14 days.
-res <- inventoryData[,sma(SalesQuantity, h=14)$forecast, 
-                     .(StoreCode, ProductCode)]
+res <- inventoryData[, mean(tail(SalesQuantity,14)), 
+                       .(StoreCode, ProductCode)]
+res <- res[rep(1:nrow(res),each=14)]
 
 # Set the date column on the forecasting results.
-res$Date <- as.character(as.Date("2017-02-01"), format="%d.%m.%Y")
+res$Date <- as.character(as.Date("2017-02-1")+ 0:13 , format="%d.%m.%Y") 
 colnames(res) <- c("StoreCode", "ProductCode","Forecast", "Date")
 setcolorder(res, c("StoreCode", "ProductCode", "Date", "Forecast"))
 
-# Check the mods 
-mods <- inventoryData[SalesQuantity != 0 & Date > 
-                        as.Date("01.01.2017", format="%d.%m.%Y"), 
-                      mean(SalesQuantity), 
-                      .(StoreCode, ProductCode)]
+# Combine the datasets with the test data and calculate the error.
+inventoryData$Forecast <- res$Forecast
+inventoryData$AbsoluteError <- abs(
+ 	as.numeric(inventoryData$SalesQuantity) - as.numeric(inventoryData$Forecast))
+forecastError <- sum(inventoryData$AbsoluteError) / sum(inventoryData$SalesQuantity)
 
 # Write the results to the output file.
 fwrite(res, outputFİlePath, quote = "auto", sep = "|")
